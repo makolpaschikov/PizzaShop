00380f5566f66fed77199050f2dd84659bbd1def
Mima <56038155+dr9ken@users.noreply.github.com>
2021-03-07 17:25:16 +0300

    Initial commit

---
+++ src/main/java/com/example/pizza_shop/PizzaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizza_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaShopApplication.class, args);
+    }
+
+}
